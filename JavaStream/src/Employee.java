666de101ee13bdf0284cda722f275f2694c34b66
Jahangir Alam <ja.dakghor@gmail.com>
2022-03-15 17:00:53 +0600

    stream flatmap
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- JavaStream/src/Employee.java
+++ JavaStream/src/Employee.java
@@ -1,6 +1,9 @@
+import java.util.stream.Stream;
 
 public class Employee {
-
+	private static Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000.0), new Employee(2, "Bill Gates", 200000.0),
+			new Employee(3, "Mark Zuckerberg", 300000.0) };
+	
 	int id;
 	String name;
 	double salary;
@@ -39,4 +42,18 @@ public class Employee {
 	public void salaryIncrement(double rate) {
 		this.salary = (1 + (rate / 100)) * this.salary;
 	}
+	
+	public static Employee getById(int id) {
+		return Stream.of(arrayOfEmps).filter(e->e.getId() == id).findFirst().orElse(null);
+	}
+	
+	public static Employee[] getAll() {
+		return arrayOfEmps;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
 }
--- JavaStream/src/InDepthStream.java
+++ JavaStream/src/InDepthStream.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InDepthStream {
-	private Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000.0), new Employee(2, "Bill Gates", 200000.0),
-			new Employee(3, "Mark Zuckerberg", 300000.0) };
 
-	private List<Employee> empList = Arrays.asList(arrayOfEmps);
+	private List<Employee> empList = Arrays.asList(Employee.getAll());
 
 	public InDepthStream() {
 
@@ -15,21 +15,55 @@ public class InDepthStream {
 	public void example_1() {
 		empList.stream().forEach(e -> System.out.println(e.salary));
 		System.out.println("salary increament 10%");
-		empList.stream().forEach(e -> {			
+		empList.stream().forEach(e -> {
 			e.salaryIncrement(10.0);
 			System.out.println(String.format("%.1f", e.salary));
 		});
 	}
-	
-	//map() api
-	//stream map() api lets convert an object to something else.
+
+	// map() api
+	// stream map() api lets convert an object to something else.
 	public void example_2() {
 		empList.stream().map(e -> e.getName()).collect(Collectors.toList()).forEach(System.out::println);
 	}
-	
-	//collect()
-	//one of the common way to get stuff out of the stream once we are done all processing.
+
+	// collect()
+	// one of the common way to get stuff out of the stream once we are done all
+	// processing.
 	public void example_3() {
 		empList.stream().collect(Collectors.toList()).forEach(System.out::println);
+	}
+
+	// filter()
+	// produce new stream from original stream with filtered data
+	public void example_4() {
+		empList.stream().filter(e -> e.getId() >= 2).forEach(e -> System.out.println(e.name));
+		empList.stream().map(e -> e.getId()).filter(e -> e.intValue() >= 2).forEach(System.out::println);
+	}
+
+	//	findFirst()
+	//	findFirst() returns an Optional for the first entry in the stream; 
+	public void example_5() {
+		Integer[] empIds = { 1, 2, 3, 4 };
+		Employee emp = Stream.of(empIds).map(Employee::getById).filter(e -> e != null)
+				.filter(e -> e.getSalary() >= 200000).findFirst().orElse(null);
+		System.out.println(emp);
+	}
+	
+	//toArray()
+	public void example_6() {
+		Employee[] array = empList.stream().toArray(Employee[]::new);
+		Stream.of(array).forEach(System.out::println);
+	}
+	
+	//flatMap()
+	//flatMap lets to flatten  the data structure to simplify further operations
+	public void example_7() {
+		List<Integer> l1 = List.of(1,2,3);
+		List<Integer> l2 = List.of(2,3,4);
+		List<Integer> l3 = List.of(3,4,5);
+		
+		Stream.of(l1,l2,l3).forEach(e-> System.out.println(e));
+		Stream.of(l1,l2,l3).distinct().flatMap(Collection::stream).forEach(System.out::println);
 	}
 }
